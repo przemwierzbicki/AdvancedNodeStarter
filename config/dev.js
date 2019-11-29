5790de0daff3ad9688580651ff02a1b34b1148e6
przem <przemichal@gmail.com>
2019-11-29 16:44:34 +0100

    update config/dev.js

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://blogster:HBynWO6zs55JbXUT@blogster-shard-00-00-anesd.mongodb.net:27017,blogster-shard-00-01-anesd.mongodb.net:27017,blogster-shard-00-02-anesd.mongodb.net:27017/test?ssl=true&replicaSet=blogster-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123'
 };
