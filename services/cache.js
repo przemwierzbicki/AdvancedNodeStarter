7e6f3987dea41ddea668d1e9341e7073247c3b08
przem <przemichal@gmail.com>
2019-12-04 16:36:02 +0100

    explore caching w/ redis #5

--- services/cache.js
+++ services/cache.js
@@ -1,4 +1,11 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+
+client.get = util.promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
 
